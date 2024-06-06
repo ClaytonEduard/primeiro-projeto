61db44af4a46b3c0312b6a00cbf903de51b12aa2
ClaytonEduard <clayton_eduard@hotmail.com>
2024-06-06 20:22:59 -0300

    Transformando  a funcao e class, so para teste

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import React from 'react'
+// Componente em cclasse é uma classe que herda a classe Component do React,
+// e retorna HTML dentro do metodo render()
+class App extends React.Component {
 
-function App() {
-  return (
-    <div>
-      <h1>Clayton Eduard</h1>
-    </div>
-  );
+
+  // metodo responsavel por renderizar o conteudo HTML do nosso componente
+  render() {
+    return (
+      <div>
+        <h1>Aprendendo React ADA</h1>
+      </div >
+    );
+  }
 }
 
 export default App;
