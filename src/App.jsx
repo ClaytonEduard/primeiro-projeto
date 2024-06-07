03584f11e757b3107e8471d42f85929f5c1f35db
ClaytonEduard <clayton_eduard@hotmail.com>
2024-06-06 21:33:58 -0300

    Diceminando os componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-import logoImg from './assets/images/logo.png'
 
 import './styles/App.css'
+import Navbar from './components/Navbar/Navbar';
 
 // Componente em cclasse é uma classe que herda a classe Component do React,
 // e retorna HTML dentro do metodo render()
@@ -10,22 +10,7 @@ class App extends React.Component {
   // metodo responsavel por renderizar o conteudo HTML do nosso componente
   render() {
     return (
-      <header>
-        <nav id="navbar">
-          <div className="nav-brand">
-            <img src={logoImg} alt='' width={80} />
-            <h1>Space Flight News</h1>
-          </div>
-
-          <ul className="nav-list">
-            <li><a href='#'>Home</a></li>
-            <li><a href='#'>Trending</a></li>
-            <li><a href='#'>Categories</a></li>
-            <li><a href='#'>About us</a></li>
-          </ul>
-
-        </nav>
-      </header>
+      <Navbar />
     );
   }
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import './styles.css'
+import NavBrand from "../NavBrand/NavBrand";
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <header>
+                <nav id="navbar">
+                    <NavBrand />
+
+                    <ul className="nav-list">
+                        <li><a href='#'>Home</a></li>
+                        <li><a href='#'>Trending</a></li>
+                        <li><a href='#'>Categories</a></li>
+                        <li><a href='#'>About us</a></li>
+                    </ul>
+
+                </nav>
+            </header>
+        )
+
+    }
+}
+
+export default Navbar;
