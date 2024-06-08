82d98faea7fc8f800dc2df7c5610cccd5dbfb3eb
ClaytonEduard <clayton_eduard@hotmail.com>
2024-06-08 11:43:59 -0300

    Finalizando o compomente Counter e trabalhando com state

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { Article } from './components/Article/Article';
 import article1Img from "./assets/images/article1.png";
 import article2Img from "./assets/images/article2.png";
 import article3Img from "./assets/images/article3.png";
+import { Counter } from './components/Counter/Counter';
 
 // Componente em cclasse é uma classe que herda a classe Component do React,
 // e retorna HTML dentro do metodo render()
@@ -17,6 +18,7 @@ class App extends React.Component {
     return (
       <>
         <Navbar />
+        <Counter />
         <section id="articles">
           <Article title="Designing Dashboards" provider="NASA" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius adipisci quo voluptatum nam vitae. Dolore eaque sed perspiciatis tenetur, fuga veritatis. Iure doloremque explicabo est, doloribus deleniti quas dolor!"
             thumbmail={article1Img}
