6a62ea2d0ee99ebfc30b8cec862349ba0ef9ccc8
Clayton Eduard <clayton_eduard@hotmail.com>
2025-03-07 14:33:01 -0300

    Entendendo o conceito de class e props

--- src/components/NavBrand/NavBrand.jsx
+++ src/components/NavBrand/NavBrand.jsx
@@ -1,14 +1,12 @@
 import React from "react";
-import logoImg from '../../assets/images/logo.png'
-import './styles.css'
+import logoImg from "../../assets/images/logo.png";
+import "./styles.css";
 
-class NavBrand extends React.Component {
-    render() {
-        return (<div className="nav-brand">
-            <img src={logoImg} alt='' width={80} />
-            <h1>Space Flight News</h1>
-        </div>)
-    }
+export function NavBrand() {
+  return (
+    <div className="nav-brand">
+      <img src={logoImg} alt="" width={80} />
+      <h1>Space Flight News</h1>
+    </div>
+  );
 }
-
-export default NavBrand;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from "react";
-import './styles.css'
-import NavBrand from "../NavBrand/NavBrand";
+import "./styles.css";
+import { NavBrand } from "../NavBrand/NavBrand";
 
-export class Navbar extends React.Component {
-    render() {
-        return (
-            <header>
-                <nav id="navbar">
-                    <NavBrand />
-
-                    <ul className="nav-list">
-                        <li><a href='#'>Home</a></li>
-                        <li><a href='#'>Trending</a></li>
-                        <li><a href='#'>Categories</a></li>
-                        <li><a href='#'>About us</a></li>
-                    </ul>
-
-                </nav>
-            </header>
-        )
-
-    }
+export function Navbar() {
+  return (
+    <header>
+      <nav id="navbar">
+        <NavBrand />
+        <ul className="nav-list">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Trending</a>
+          </li>
+          <li>
+            <a href="#">Categories</a>
+          </li>
+          <li>
+            <a href="#">About us</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
