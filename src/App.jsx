c2db7a77a962d97d866b5ee5434e3a3a6f05a3eb
Clayton Eduard <clayton_eduard@hotmail.com>
2025-03-13 19:31:50 -0300

    Aprendendo conceito de useEffect, pacote yarn e loader spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,70 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-
-import './styles/App.css'
-import { Navbar } from './components/Navbar/Navbar';
-import { Article } from './components/Article/Article';
-
-import article1Img from "./assets/images/article1.png";
-import article2Img from "./assets/images/article2.png";
-import article3Img from "./assets/images/article3.png";
-import { Counter } from './components/Counter/Counter';
+import "./styles/App.css";
+import { Navbar } from "./components/Navbar/Navbar";
+import { Article } from "./components/Article/Article";
+import axios from "axios";
+import { ThreeDots } from "react-loader-spinner";
 
 // Componente em cclasse é uma classe que herda a classe Component do React,
 // e retorna HTML dentro do metodo render()
-class App extends React.Component {
-  // metodo responsavel por renderizar o conteudo HTML do nosso componente
-  render() {
-    return (
-      <>
-        <Navbar />
-        <Counter />
-        <section id="articles">
-          <Article title="Designing Dashboards" provider="NASA" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius adipisci quo voluptatum nam vitae. Dolore eaque sed perspiciatis tenetur, fuga veritatis. Iure doloremque explicabo est, doloribus deleniti quas dolor!"
-            thumbmail={article1Img}
+function App() {
+  const [news, setNews] = useState([]);
 
-          />
+  useEffect(() => {
+    async function fetchNews() {
+      const response = await axios.get(
+        "https://api.spaceflightnewsapi.net/v4/articles/"
+      );
+      const data = await response.data.results;
+      console.log(data);
+      setNews(data);
+    }
+    fetchNews();
+    console.log(news);
+  }, []);
 
-          <Article title="Vibrant Portraits of 2020" provider="SpaceNews" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius adipisci quo voluptatum nam vitae. Dolore eaque sed perspiciatis tenetur, fuga veritatis. Iure doloremque explicabo est, doloribus deleniti quas dolor!"
-            thumbmail={article2Img} />
+  return (
+    <>
+      <Navbar />
 
-          <Article title="36 Days of Malayalam type" provider="Spaceflight Now" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius adipisci quo voluptatum nam vitae. Dolore eaque sed perspiciatis tenetur, fuga veritatis. Iure doloremque explicabo est, doloribus deleniti quas dolor!"
-            thumbmail={article3Img} />
-
-          <Article title="Designing Dashboards" provider="NASA" description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque eius adipisci quo voluptatum nam vitae. Dolore eaque sed perspiciatis tenetur, fuga veritatis. Iure doloremque explicabo est, doloribus deleniti quas dolor!" thumbmail={article1Img} />
-        </section>
-      </>
-    );
-  }
+      {/* <Counter /> */}
+      <section id="articles">
+        {news.length === 0 ? (
+          <div
+            style={{
+              display: "flex",
+              height: "400px",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <ThreeDots
+              height="200"
+              width="200"
+              radius="9"
+              color="white"
+              ariaLabel="loading"
+            />
+          </div>
+        ) : (
+          news.map((article) => {
+            return (
+              <Article
+                key={article.id}
+                url={article.url}
+                title={article.title}
+                provider={article.news_site}
+                description={article.summary}
+                thumbmail={article.image_url}
+              />
+            );
+          })
+        )}
+      </section>
+    </>
+  );
 }
 
 export default App;
--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./styles.css";
 
-export function Article({ title, thumbmail, provider, description }) {
+export function Article({url, title, thumbmail, provider, description }) {
   return (
     <article id="article">
-      <img src={thumbmail} width={200} alt={title} />
+      <img src={thumbmail} alt={title} />
       <div className="article-infos">
-        <h2>{title}</h2>
+        <a href={url} target="_blank" rel="noreferrer">
+          <h2>{title}</h2>
+        </a>
         <h3>{provider}</h3>
         <p>{description}</p>
       </div>
